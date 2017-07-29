8f6af6506f8b4aea6f0b154b4b1a58a509ddf910
Ryan Jones <raj8r8@Ryans-MacBook-Pro.local>
2017-07-28 20:24:20 -0500

    Added button functionality

--- ConversionCalculator/ConversionCalculator/ConversionCalculatorViewController.swift
+++ ConversionCalculator/ConversionCalculator/ConversionCalculatorViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ConversionCalculatorViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +23,60 @@ class ConversionCalculatorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func button7(_ sender: UIButton) {
+        inputField.text = inputField.text! + "7"
+    }
 
+    @IBAction func button8(_ sender: Any) {
+        inputField.text = inputField.text! + "8"
+    }
+    
+    @IBAction func button9(_ sender: UIButton) {
+        inputField.text = inputField.text! + "9"
+    }
+    
+    @IBAction func button4(_ sender: UIButton) {
+        inputField.text = inputField.text! + "4"
+    }
+    
+    @IBAction func button5(_ sender: UIButton) {
+        inputField.text = inputField.text! + "5"
+    }
+    
+    @IBAction func button6(_ sender: UIButton) {
+        inputField.text = inputField.text! + "6"
+    }
+    
+    @IBAction func button1(_ sender: UIButton) {
+        inputField.text = inputField.text! + "1"
+    }
+    
+    @IBAction func button2(_ sender: UIButton) {
+        inputField.text = inputField.text! + "2"
+    }
+    
+    @IBAction func button3(_ sender: UIButton) {
+        inputField.text = inputField.text! + "3"
+    }
+    
+    @IBAction func button0(_ sender: UIButton) {
+    inputField.text = inputField.text! + "0"
+    }
+    
+    @IBAction func buttondec(_ sender: UIButton) {
+        inputField.text = inputField.text! + "."
+    }
+    
+    @IBAction func buttonClear(_ sender: UIButton) {
+        inputField.text = ""
+    }
+    
+    @IBAction func buttonposneg(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonConvert(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
